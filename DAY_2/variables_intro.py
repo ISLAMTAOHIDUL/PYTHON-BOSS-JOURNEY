c1bd2ad17e7847c27ee02d70560352a8130b3b30
Taohidul Islam <mdtaohidbhuiyan72@gmail.com>
2026-04-05 01:50:43 +0900

    Complete Day-2 Python Basics

---
+++ DAY_2/variables_intro.py
@@ -0,0 +1,15 @@
+name = "Taohid"
+country = "Bangladesh"
+current_country = "South Korea"
+university = "DEU"
+age = 22
+phone_number = "010-6461-5687"
+
+
+
+print (name)
+print (country)
+print(current_country)
+print(university)
+print(age)
+print(phone_number)
---
+++ data_types.py
@@ -0,0 +1,18 @@
+name = "Taohid"
+age = 22
+gpa = 4.39
+is_student = True
+
+
+
+print("Name:", name)
+print("Age:", age)
+print("Cgpa:", gpa)
+print("Is a student?", is_student)
+
+
+
+print(type(name))
+print(type(age))
+print(type(gpa))
+print(type(is_student))
