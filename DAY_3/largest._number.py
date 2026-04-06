5257787b423c94de967cf5d15fd1fe03f58393e0
Taohidul Islam <mdtaohidbhuiyan72@gmail.com>
2026-04-07 01:59:13 +0900

    Uploading largest_number file

---
+++ DAY_3/largest._number.py
@@ -0,0 +1,9 @@
+number1 = input("Enter your first number: ")
+number2 = input("Enter your second number: ")
+
+if number1 > number2:
+    print("The largest number is: ", number1)
+elif number2 > number1:
+    print("The largest number is: ", number2)
+else:
+    print("The numbers are equal.")
