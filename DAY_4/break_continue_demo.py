ba2ce33b2d2fc3c815fa0b0b2b23d1b7d8614cd8
Taohidul Islam <mdtaohidbhuiyan72@gmail.com>
2026-04-13 03:57:49 +0900

    Update inside break_continue_demo file

--- DAY_4/break_continue_demo.py
+++ DAY_4/break_continue_demo.py
@@ -1,4 +1,13 @@
 for i in range(1, 10):
     if i == 5:
-        break
+        break #after 4 the loop wilol break
+        continue # just skip 5 and continue
+    print(i)
+
+
+print("-----------------------------------------")
+
+for i in range(1, 10):
+    if i == 5:
+        continue # just skip 5 and continue
     print(i)
